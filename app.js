aa55715a29500499115a156e46c6665e749af6ab
Luis Mayta <luis.mayta@gmail.com>
2020-06-08 18:23:20 -0500

    Clase 5

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
 "use strict";
 (function () {
-    function getEdad() {
-        return 1 + 2 + 3;
-    }
-    var nombre = "Luis";
-    var apellido = "Mayta";
-    var edad = 40;
-    // const salida = nombre + " "+ apellido+" ("+edad+")"
-    var salida = nombre + " " + apellido + " (" + edad + ") fun: " + getEdad();
-    console.log(salida);
 })();
