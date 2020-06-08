1137006a1cf3935154c1e2bd50d03a283b7c863f
Luis Mayta <luis.mayta@gmail.com>
2020-06-08 17:44:16 -0500

    Clase 3

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-function saludar(nombre) {
-    console.table('Hola ' + nombre); // Hola Logan
-}
-var wolverine = {
-    nombre: 'Logan'
-};
-saludar(wolverine.nombre);
+"use strict";
+(function () {
+    function saludar(nombre) {
+        console.table('Hola ' + nombre); // Hola Logan
+    }
+    var wolverine = {
+        nombre: 'Logan'
+    };
+    saludar(wolverine.nombre);
+    var numero = 1;
+    var letra = 'abc';
+    var booleano = true;
+    var hoy = new Date();
+    var spiderman = {
+        nombre: 'Peter',
+        edad: 30
+    };
+})();
