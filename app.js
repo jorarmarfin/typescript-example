80f3efdb0682d8993bcd85503adcb13ed1394817
Luis Mayta <luis.mayta@gmail.com>
2020-06-10 00:26:48 -0500

    clase 7

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 "use strict";
 (function () {
-    var miFuncion = function (a) {
-        return a.toUpperCase();
+    var avenger = {
+        nombre: 'steve',
+        clave: 'capitan america',
+        poder: 'droga'
     };
-    var miFuncionF = function (a) { return a.toUpperCase(); };
-    var sumarN = function (a, b) {
-        return a + b;
+    console.log(avenger.nombre);
+    console.log(avenger.clave);
+    console.log(avenger.poder);
+    var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
+    console.log(nombre);
+    console.log(clave);
+    console.log(poder);
+    //Destructuracion como argumento
+    var extraer = function (_a) {
+        var nombre = _a.nombre, clave = _a.clave;
+        console.log(nombre);
+        console.log(clave);
     };
-    var sumarF = function (a, b) { return a + b; };
-    console.log(sumarF(1, 2));
-    var hulk = {
-        nombre: 'hulk',
-        smash: function () {
-            console.log(this.nombre + " smash!!");
-        }
+    extraer(avenger);
+    var avengers = ['thor', 'ironmar', 'spiderman'];
+    console.log(avengers[0]);
+    console.log(avengers[1]);
+    console.log(avengers[2]);
+    var uno = avengers[0], dos = avengers[1], tres = avengers[2];
+    var cuatro = avengers[2]; //no me interesa los primeros
+    console.log(uno);
+    console.log(dos);
+    console.log(tres);
+    var extraerArray = function (_a) {
+        var cinco = _a[0], seis = _a[1], siete = _a[2];
+        console.log(cinco);
+        console.log(seis);
     };
-    var hulkF = {
-        nombre: 'hulk',
-        smash: function () {
-            var _this = this;
-            setTimeout(function () {
-                console.log(_this.nombre + " smash!!");
-            }, 1000);
-        }
-    };
+    extraerArray(avengers);
 })();
